64a1f5107b489c9de96d6b9aa7449aca580c2a35
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-21 12:44:24 +0530

    using httprouter

--- 08-http-services/01-net-http/server-02.go
+++ 08-http-services/01-net-http/server-02.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -16,6 +17,15 @@ func log(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWri
 	}
 }
 */
+
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+func chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
 
 //middleware
 func log(handler http.HandlerFunc) http.HandlerFunc {
@@ -38,9 +48,40 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World!")
 }
 
+type Product struct {
+	Id   int     `json:"id"`
+	Name string  `json:"name"`
+	Cost float32 `json:"cost"`
+}
+
+var products = []Product{
+	{Id: 101, Name: "Pen", Cost: 10},
+	{Id: 102, Name: "Pencil", Cost: 20},
+}
+
 func productsHandler(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(2 * time.Second)
-	fmt.Fprintln(w, "Product requests are processed")
+	switch r.Method {
+	case "GET":
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(products); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	case "POST":
+		decoder := json.NewDecoder(r.Body)
+		var newProduct Product
+		if err := decoder.Decode(&newProduct); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		newProduct.Id = len(products) + 101
+		products = append(products, newProduct)
+		w.WriteHeader(http.StatusCreated)
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(newProduct); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	default:
+		fmt.Fprintf(w, "Products handler executed")
+	}
 }
 
 func customersHandler(w http.ResponseWriter, r *http.Request) {
@@ -49,8 +90,13 @@ func customersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", profile(log(indexHandler)))
-	http.HandleFunc("/products", profile(log(productsHandler)))
-	http.HandleFunc("/customers", profile(log(customersHandler)))
+	/*
+		http.HandleFunc("/", profile(log(indexHandler)))
+		http.HandleFunc("/products", profile(log(productsHandler)))
+		http.HandleFunc("/customers", profile(log(customersHandler)))
+	*/
+	http.HandleFunc("/", chain(indexHandler, log, profile))
+	http.HandleFunc("/products", chain(productsHandler, log, profile))
+	http.HandleFunc("/customers", chain(customersHandler, log, profile))
 	http.ListenAndServe(":8080", nil)
 }
